01882552471b39b4be8d2a9f735005e0e9541e5b
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-07-12 13:59:27 -0500

    created the schma and tables for each reuired table

--- tracker.sql
+++ tracker.sql
@@ -1,11 +1,25 @@
 -- /* Schema for SQL database/table.
-
+DROP DATABASE IF EXISTS tracker;
 -- /* Create database */
-
+CREATE DATABASE tracker;
+USE tracker;
 -- /* Create EMPLOYEES table with a primary key of ID that auto-increments  */
-
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEYS (role_id, manager_id)
+);
 -- also the foreign keys of role and manager ID's
 
 -- Create ROLES table with a primary key of ID and foreign key of department_id
-
+CREATE TABLE roles (
+    title VARCHAR(30) NOT NULL
+    dept_id INT NOT NULL AUTO_INCREMENT FOREIGN KEY,
+);
 -- Create DEPARTMENTS table with just a text field with the department names
+CREATE TABLE departments (
+    name VARCHAR(30)
+);
