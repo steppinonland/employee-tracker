e8ce7da5d82c36564975bb375de8f72cb593a996
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-07-11 22:17:30 -0500

    pseudocoded

---
+++ tracker.sql
@@ -0,0 +1,11 @@
+-- /* Schema for SQL database/table.
+
+-- /* Create database */
+
+-- /* Create EMPLOYEES table with a primary key of ID that auto-increments  */
+
+-- also the foreign keys of role and manager ID's
+
+-- Create ROLES table with a primary key of ID and foreign key of department_id
+
+-- Create DEPARTMENTS table with just a text field with the department names
