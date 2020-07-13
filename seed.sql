2449e1f6257aa735c3bbfd52d20b8617d03cf439
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-07-12 21:28:53 -0500

    got the CLI to work and the SQL authenticated right

--- seed.sql
+++ seed.sql
@@ -1,7 +1,29 @@
 -- seeds for SQL tables
-
+USE tracker;
 -- inserting the employees
+INSERT INTO employees (first_name, last_name)
+VALUES ("Fiona", "Lamar"),
+("Fred", "Lowe"),
+("Florence", "Leavy"),
+("Frank", "Lombardi"),
+("Felix", "Lopez"),
+("Francine", "Lockhart"),
 
 -- inserting the roles
 
+INSERT INTO roles (name)
+VALUES ("Sales Lead"), 
+        ("Salesperson"), 
+        ("Lead Engineer"), 
+        ("Software Engineer"), 
+        ("Accountant"), 
+        ("Lawyer")
+
 -- inserting the departments
+INSERT INTO departments (name)
+VALUES ("Sales"), 
+        ("Finance"), 
+        ("Engineering"), 
+        ("Legal"), 
+        ("Accounting"), 
+        ("HR")
